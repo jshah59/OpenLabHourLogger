c833bca69062386e4bf4e0546d7448eef55b75d1
Jayam <jshah59@illinois.edu>
2018-08-29 22:33:32 -0500

    Initial commit. Setup Selenium library and outlined params.

---
+++ src/HourLogger.java
@@ -0,0 +1,25 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.WebElement;
+
+public class HourLogger {
+
+    public static void main(String[] args) {
+        // Need username, password, day, start, end
+        if (args.length != 5) {
+            System.out.println("Invalid arguments!");
+            return;
+        }
+
+        String username = args[0];
+        String password = args[1];
+        String day = args[2];
+        String startTime = args[3];
+        String endTime = args[4];
+
+        WebDriver driver =  new ChromeDriver();
+        driver.get("https://my.ece.illinois.edu/portal/Account/Login");
+
+        
+    }
+}
